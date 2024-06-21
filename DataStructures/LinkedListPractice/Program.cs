aea045ac98ad586ff68dcb620e4fec37d4e9e1ed
afishlover <trungbuiducbuiduc@gmail.com>
2024-06-21 13:25:05 +0700

    Practice Singly Linked List

---
+++ DataStructures/LinkedListPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using LinkedListPractice.SinglyLinkedListImplementation;
+
+namespace LinkedListPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var singlyLinkedList = new SinglyLinkedList<int>();
+            singlyLinkedList.Head = new Node<int> { Data = -1 };
+
+            for (int i = 1; i < 10; i++)
+            {
+                singlyLinkedList.AddLast(i);
+            }
+
+
+            Console.WriteLine(singlyLinkedList.ToString());
+            singlyLinkedList.InsertBeforeNode(0, singlyLinkedList.GetNodeByIndex(9));
+            Console.WriteLine(singlyLinkedList.ToString());
+            singlyLinkedList.DeleteByIndex(9);
+            Console.WriteLine(singlyLinkedList.ToString());
+            singlyLinkedList.InsertManyAtIndex(3, new int[] { -100, 1, 2, 3, 4, -500 });
+            Console.WriteLine(singlyLinkedList.ToString());
+        }
+    }
+}
---
+++ DataStructures/LinkedListPractice/SinglyLinkedListImplementation/SinglyLinkedList.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListPractice.SinglyLinkedListImplementation
+{
+    public class SinglyLinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+
+        // Get the size of the list
+        public int Size()
+        {
+            var result = 0;
+            var start = Head;
+            while (start != null)
+            {
+                result++;
+                if (start.Next == null)
+                {
+                    break;
+                }
+                else
+                {
+                    start = start.Next;
+                }
+            }
+            Console.WriteLine($"This list has size of {result}");
+            return result;
+        }
+        
+        // Add multiple element at index
+        public int InsertManyAtIndex(int index, params T[] dataNode)
+        {
+            if (Head == null)
+            {
+                return -1;
+            }
+
+            if (index < 0  || index >= Size()) 
+            {
+                return -1;
+            }
+
+            var start = Head;
+            Node<T> prevNode = null;
+            var counter = 0;
+            while (start != null && counter < index)
+            {
+                counter++;
+                prevNode = start;
+                start = start.Next;
+            }
+
+            foreach (var data in dataNode)
+            {
+                InsertAfterNode(data, prevNode);
+            }
+
+            return 1;
+
+        }
+
+        // Delete at specific index
+        public int DeleteByIndex(int index)
+        {
+            if (Head == null)
+            {
+                return -1;
+            }
+
+            if (index < 0 || index >= Size())
+            {
+                return -1;
+            }
+
+            if (index == 0)
+            {
+                Head = Head.Next;
+                return 1;
+            }
+
+            var start = Head;
+            var counter = 0;
+            Node<T> prevNode = null;
+
+            while (start != null && counter < index)
+            {
+                prevNode = start;
+                start = start.Next;
+                counter++;
+            }
+
+            if (start == null)
+            {
+                return -1;
+            }
+
+            prevNode.Next = start.Next;
+            start = null;
+
+            return 1;
+        }
+
+        // Get node at specific index
+        public Node<T> GetNodeByIndex(int index)
+        {
+            Node<T>? result = null;
+            if (index >= Size())
+            {
+                return null;
+            }
+
+            var start = Head;
+            var counter = 0;
+
+            while (start != null)
+            {
+                if (counter == index)
+                {
+                    result = start;
+                }
+                counter++;
+                start = start.Next;
+            }
+            return result;
+        }
+
+        // Insert a new node before a specific node
+        public void InsertBeforeNode(T data, Node<T> node)
+        {
+            if (Head == null)
+            {
+                return;
+            }
+
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.Equals(Head))
+            {
+                AddFirst(data);
+                return;
+            }
+
+            var start = Head;
+            while (start != null)
+            {
+                if (node.Equals(start.Next))
+                {
+                    var newNode = new Node<T> { Data = data };
+                    newNode.Next = node;
+                    start.Next = newNode;
+                    break;
+                }
+                start = start.Next;
+            }
+        }
+
+        // Insert a new node after a specific node
+        public void InsertAfterNode(T data, Node<T> node)
+        {
+            if (Head == null)
+            {
+                return;
+            }
+
+            if (node == null)
+            {
+                return;
+            }
+
+            var start = Head;
+            while (start != null)
+            {
+                if (start.Equals(node))
+                {
+                    var newNode = new Node<T> { Data = data };
+                    newNode.Next = start.Next;
+                    start.Next = newNode;
+                    return;
+                }
+                start = start.Next;
+            }
+        }
+
+        // Add new node at the begining of the list
+        public void AddFirst(T data)
+        {
+            var newHead = new Node<T> { Data = data };
+            newHead.Next = Head;
+            Head = newHead;
+        }
+
+        // Add new node at the end of the list
+        public void AddLast(T data)
+        {
+            if (Head == null)
+            {
+                Head = new Node<T> { Data = data };
+                return;
+            }
+
+            var start = Head;
+            while (start != null)
+            {
+                if (start.Next == null)
+                {
+                    start.Next = new Node<T> { Data = data };
+                    break;
+                }
+                else
+                {
+                    start = start.Next;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var result = new List<T>();
+
+            if (Head == null)
+            {
+                return "[]";
+            }
+
+            var start = Head;
+            while (start != null)
+            {
+                result.Add(start.Data);
+                start = start.Next;
+            }
+
+            return $"[{string.Join(", ", result.ToArray())}]";
+        }
+    }
+}
